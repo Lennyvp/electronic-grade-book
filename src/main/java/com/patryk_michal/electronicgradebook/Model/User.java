b30b0b92dc81d0dbdfc72b7ae7c4c0f95ba9cc0c
Michal <lasek3@example.com>
2019-10-27 14:07:40 +0100

    Adding Some Models

---
+++ src/main/java/com/patryk_michal/electronicgradebook/Model/User.java
@@ -0,0 +1,11 @@
+package com.patryk_michal.electronicgradebook.Model;
+
+public abstract class User {
+
+    private int ID;
+    private String login;
+    private String password;
+    private String name;
+    private String surname;
+
+}
