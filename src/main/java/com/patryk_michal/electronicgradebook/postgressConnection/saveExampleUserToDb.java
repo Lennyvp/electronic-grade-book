ef1d70f157973a645d68c0c361df6c3e34a3a496
Michal <lasek3@example.com>
2019-10-27 21:01:55 +0100

    Simple PostgresSQL configuration

--- src/main/java/com/patryk_michal/electronicgradebook/postgressConnection/saveExampleUserToDb.java
+++
@@ -1,8 +0,0 @@
-package com.patryk_michal.electronicgradebook.postgressConnection;
-
-import com.patryk_michal.electronicgradebook.Model.Student;
-
-public class saveExampleUserToDb {
-    Student student = new Student("login","password","examplename","examplesurname");
-
-}
