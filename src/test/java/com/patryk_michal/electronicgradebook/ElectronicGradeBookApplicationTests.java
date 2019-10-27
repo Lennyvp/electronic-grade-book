5e1c019f9aba6768d4143a46075267303873ef24
Lennyvp <at3poweredition@gmail.com>
2019-10-27 12:56:06 +0100

    Initial commit

---
+++ src/test/java/com/patryk_michal/electronicgradebook/ElectronicGradeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patryk_michal.electronicgradebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicGradeBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
