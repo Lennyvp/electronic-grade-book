8c3ce7a5443604de74ed182a379555e1ecdcc2ce
Michal <lasek3@gmail.com>
2019-12-01 17:42:05 +0100

    Added IdController

--- src/main/java/com/patryk_michal/electronicgradebook/postgressConnection/student/StudentRepository.java
+++ src/main/java/com/patryk_michal/electronicgradebook/postgressConnection/student/StudentRepository.java
@@ -4,4 +4,5 @@ import com.patryk_michal.electronicgradebook.model.Student;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
+    Student findStudentByLogin(String login);
 }
