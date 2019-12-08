e0f3fb56a11715b4e5b56c58133e931c21fb674d
Michal <lasek3@gmail.com>
2019-12-08 13:11:07 +0100

    Added more subjects and grades

--- src/main/java/com/patryk_michal/electronicgradebook/postgressConnection/dataLoader/DataLoader.java
+++ src/main/java/com/patryk_michal/electronicgradebook/postgressConnection/dataLoader/DataLoader.java
@@ -42,10 +42,16 @@ public class DataLoader implements ApplicationRunner {
         Subject polish = new Subject("Polski");
         Subject english = new Subject("Angielski");
         Subject maths = new Subject("Matematyka");
+        Subject physics = new Subject("Fizyka");
+        Subject biology = new Subject("Biologia");
+        Subject chemistry = new Subject("Chemia");
 
         subjectRepository.save(polish);
         subjectRepository.save(english);
         subjectRepository.save(maths);
+        subjectRepository.save(physics);
+        subjectRepository.save(biology);
+        subjectRepository.save(chemistry);
 
         Grade grade1 = new Grade(5,LocalDateTime.now(),student1, polish);
         Grade grade2 = new Grade(3,LocalDateTime.now(),student2,polish);
@@ -57,6 +63,15 @@ public class DataLoader implements ApplicationRunner {
         Grade grade8 = new Grade(3,LocalDateTime.now(),student1, polish);
         Grade grade9 = new Grade(4,LocalDateTime.now(),student1, polish);
         Grade grade10 = new Grade(5,LocalDateTime.now(),student1, polish);
+        Grade grade11 = new Grade(5,LocalDateTime.now(),student1, physics);
+        Grade grade12 = new Grade(5,LocalDateTime.now(),student1, physics);
+        Grade grade13 = new Grade(4,LocalDateTime.now(),student1, physics);
+        Grade grade14 = new Grade(6,LocalDateTime.now(),student1, biology);
+        Grade grade15 = new Grade(5,LocalDateTime.now(),student1, biology);
+        Grade grade16 = new Grade(3,LocalDateTime.now(),student1, chemistry);
+        Grade grade17 = new Grade(4,LocalDateTime.now(),student1, chemistry);
+        Grade grade18 = new Grade(2,LocalDateTime.now(),student1, maths);
+
 
         gradeRepository.save(grade1);
         gradeRepository.save(grade2);
@@ -68,6 +83,14 @@ public class DataLoader implements ApplicationRunner {
         gradeRepository.save(grade8);
         gradeRepository.save(grade9);
         gradeRepository.save(grade10);
+        gradeRepository.save(grade11);
+        gradeRepository.save(grade12);
+        gradeRepository.save(grade13);
+        gradeRepository.save(grade14);
+        gradeRepository.save(grade15);
+        gradeRepository.save(grade16);
+        gradeRepository.save(grade17);
+        gradeRepository.save(grade18);
 
     }
 }
